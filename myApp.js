af84492e98e53c0dfc3659550b592cae1912b9dc
Krystle <krystle@Mac.lan>
2025-11-19 22:31:57 -0800

    Defined message

--- myApp.js
+++ myApp.js
@@ -14,7 +14,9 @@ app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
   res.json({
-  message: process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+  message: process.env.MESSAGE_STYLE === "uppercase"
+   ? message.toUppercase()
+    : message
   });
 });  
 
