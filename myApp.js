aaeadac46a90f867dff968d396f6360e9538c9bf
Krystle <krystle@Mac.lan>
2025-11-19 21:51:56 -0800

    Typo

--- myApp.js
+++ myApp.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-res.json({
-  message: 'Hello Json'
+  res.json({
+  message: 'Hello json'
   });
 });  
 
