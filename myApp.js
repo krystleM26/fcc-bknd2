153163e6681bd25d5979d7f14d3db17373eb2dea
Krystle <krystle@Mac.lan>
2025-11-24 19:11:37 -0800

    Updated if-statement

--- myApp.js
+++ myApp.js
@@ -14,15 +14,14 @@ app.use("/public", express.static(__dirname + "/public"));
 
 var message = "Hello json";
 app.get("/json", (req, res) => {
-  if  (process.env.MESSAGE_STYLE === "uppercase") {
-    message = message.toUpperCase();
-  } else {
-    message = "Hello JSON";
+  
+    if ( process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({ "message": message.toUpperCase() });
+  }
+  else {
+    res.json({ "message": message });
   }
 
-    res.json({ 
-      message: "message" }); // Corrected 'messange' to 'message'
-  
 });
  
 
