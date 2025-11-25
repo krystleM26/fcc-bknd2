d59c1cc1316c0dce45c872f032cf27122f08e290
Krystle <krystle@Mac.lan>
2025-11-24 18:56:34 -0800

    Starting over

--- myApp.js
+++ myApp.js
@@ -4,21 +4,18 @@ let app = express();
 
 
 
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 app.use("/public", express.static(__dirname + "/public"));
 
-var message = 'Hello json';
-console.log(message)
+
+
 app.get("/json", (req, res) => {
-  if ( process.env['MESSAGE_STYLE'] === "uppercase") {
-      res.json({ "message": message.toUpperCase() });
-  }
-  else {
-    res.json({ "message": message }); // Corrected 'messange' to 'message'
-  }
+    res.json({ "message": "Hello Json" }); // Corrected 'messange' to 'message'
+  
 });
  
 
